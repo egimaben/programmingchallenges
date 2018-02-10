32377c860df4da581745a946c3db46491a9b88ea
egimaben <egimaben@gmail.com>
2018-02-10 18:48:56 +0300

    finished solving the 3n+1 problem

--- src/com/egimaben/skiena/starter/CycleProblem.java
+++ src/com/egimaben/skiena/starter/CycleProblem.java
@@ -17,16 +17,15 @@ public class CycleProblem {
             int sequenceInput = i;
             int cycleLen = 0;
             do {
-                int tmp=sequenceInput;
                 if (sequenceInput % 2 == 0) {
                     sequenceInput = sequenceInput / 2;
                 } else if (sequenceInput % 2 == 1) {
                     sequenceInput = 3 * sequenceInput + 1;
                 }
-                System.out.println(tmp+"----->"+sequenceInput);
                 cycleLen++;
             } while (sequenceInput != 1);
-            System.out.println("cycle len for "+i+" is "+cycleLen);
+            if(sequenceInput==1)
+                cycleLen++;
             if (cycleLen > maxLen) {
                 maxLen = cycleLen;
             }
@@ -35,8 +34,8 @@ public class CycleProblem {
     }
 
     public static void main(String[] args) {
-        int i=10;
-        int j=22;
+        int i=900;
+        int j=1000;
         System.out.println("Max cycle len of "+i+" and "+j+" is "+getMaxCycleLength(i, j));
     }
 
